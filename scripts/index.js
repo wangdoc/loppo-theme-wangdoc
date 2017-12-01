fb6af95d8c71227b96bb887500757a0e000ca88a
ruanyf <yifeng.ruan@gmail.com>
2017-12-01 15:04:14 +0800

    style: edit script

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,26 @@
 /* set content's min-height */
-/*
 (function () {
-  var content = document.querySelector('article.content');
+  var content = document.querySelector('.article');
   var navbarHeight =
     window.getComputedStyle(document.querySelector('.navbar'))
     .getPropertyValue('height');
+
+  var heroHeight = 0;
+  if (document.querySelector('.hero')) {
+    var heroHeight =
+      window.getComputedStyle(document.querySelector('.hero'))
+      .getPropertyValue('height');
+  }
+
   var footHeight =
     window.getComputedStyle(document.querySelector('.footer'))
     .getPropertyValue('height');
   var minHeight = document.documentElement.clientHeight
     - parseInt(navbarHeight)
+    - parseInt(heroHeight)
     - parseInt(footHeight);
   content.style.minHeight = minHeight + 'px';
 })();
-*/
 
 /* insert article's toc */
 (function () {
